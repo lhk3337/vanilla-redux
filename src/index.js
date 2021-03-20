c53382e2edd67a9699dc38a8b7786e59ee649d55
lhk3337 <lhk3337@naver.com>
2021-03-20 13:33:02 +0900

    add action ADD and MINUS

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 const reducer = (count = 0, action) => {
+  console.log(count, action);
   if (action.type === "ADD") {
-    console.log("they are telling me to add one");
     return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
   }
-  return count;
 };
 
 const store = createStore(reducer);
-store.dispatch({ type: "ADD" });
+
+store.dispatch({ type: "ADD" }); //count = 1, return + 1
+store.dispatch({ type: "ADD" }); //count = 2, return + 1
+store.dispatch({ type: "ADD" }); //count = 3, return + 1
+store.dispatch({ type: "ADD" }); //count = 4, return + 1
+store.dispatch({ type: "ADD" }); //count = 5, return + 1
+store.dispatch({ type: "MINUS" }); //count = 4, return - 1
+
 console.log(store.getState());
