6a6be3e88a357952b72af98181a577fd9a7b56d3
lhk3337 <lhk3337@naver.com>
2021-03-22 00:55:52 +0900

    리덕스에서 상태변경금지

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      // return state.push(action.text); //state mutation 상태는 리덕스에서 사용하지 않는다.
+      //다음 리듀서 상태를 리턴할때는 mutating(수정) 상태 대신에 새로운 상태 객체를 리턴해야 한다.
+      return [...state, { text: action.text }]; //새로운 상태 객체를 리턴
     case DELETE_TODO:
       return [];
     default:
@@ -20,11 +22,13 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
+store.subscribe(() => console.log(store.getState()));
+
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  store.dispatch({ type: ADD_TODO, text: toDo, id: Date.now() });
 };
 
 form.addEventListener("submit", onSubmit);
